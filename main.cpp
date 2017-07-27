302e87ec192c0421dd78b1cd4e718bb44872da3c
Nabil Elqatib <n.elqatib@catie.fr>
2017-07-27 17:14:39 +0200

    Update main program

--- main.cpp
+++ main.cpp
@@ -15,18 +15,45 @@
  * limitations under the License.
  */
 #include "mbed.h"
+#include "pb.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "environment.pb.h"
 
 namespace {
 #define PERIOD_MS 500
 }
 
-static DigitalOut led1(LED1);
+uint8_t buf_in[100], buf_out[100];
+static Environment env_in, env_out;
 
-// main() runs in its own thread in the OS
-// (note the calls to Thread::wait below for delays)
 int main()
 {
+    env_in.temperature = 25.0;
+    env_in.humidity = 57.6;
+    env_in.pressure = 1013.0;
+
+    pb_ostream_t stream_out = pb_ostream_from_buffer(buf_out, sizeof(buf_out));
+    pb_istream_t stream_in = pb_istream_from_buffer(buf_in, sizeof(buf_in));
+
     while (true) {
-        printf("Alive!\n");
+        printf("\nAlive!\n");
+        printf("Encoding...\n");
+        if (!pb_encode(&stream_out, Environment_fields, &env_in)){
+            printf("Encoding failed: %s\n", PB_GET_ERROR(&stream_out));
+            continue;
+        }
+        printf("%s\n", buf_out);
+
+        printf("Decoding...\n");
+        for (int i = 0; i < stream_out.bytes_written; ++i)
+            buf_in[i] = buf_out[i];
+        if (!pb_decode(&stream_in, Environment_fields, &env_in)){
+            printf("Decoding failed: %s\n", PB_GET_ERROR(&stream_in));
+            continue;
+        }
+        printf("Decoded values: temperature: %f , humidity: %f , pressure: %f\n", env_in.temperature, env_in.humidity, env_in.pressure);
+
+        wait(PERIOD_MS);
     }
 }
